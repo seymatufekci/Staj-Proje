fda7672427968adf5bc87fd3a0740ad9f33381ec
Seyma Tufekci <73655124+seymatufekci@users.noreply.github.com>
2021-07-06 13:22:06 +0300

    Add files via upload

---
+++ arayuz_kod.py
@@ -0,0 +1,21 @@
+def arayuz_1():
+    print("-----------------------------------------------------------------")
+    print("Guncel web sayfasi taramak icin - 1")
+    print("Wayback Machine kullanmak icin - 2")
+    print("-----------------------------------------------------------------")
+
+def arayuz_2():
+    print("-----------------------------------------------------------------")
+    print("Wayback Machinede bulunan en eski sayfaya ulasmak icin - 1")
+    print("Wayback Machinede bulunan en yeni sayfaya ulasmak icin - 2")
+    print("Wayback Machinede istediginiz yila ait sayfaya ulasmak icin - 3")
+    print("Wayback Machinede belirli bir tarihe ait sayfaya ulasmak icin - 4")
+    print("-----------------------------------------------------------------")
+
+def arayuz_3():
+    print("-----------------------------------------------------------------")
+    print("Aradiginiz sayfa icin Wayback Machine'de veri bulunamadi.")
+    print("Aradiginiz sayfayi archive.org'a kaydetmek icin - 1")
+    print("Cikis yapmak icin - 2")
+    print("-----------------------------------------------------------------")
+
